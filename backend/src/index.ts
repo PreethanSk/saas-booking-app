a6fceae420da1302b2f25fbc19633cbf7f7a1b25
Preethan Sk <preethansk1@gmail.com>
2025-04-17 14:32:19 +0530

    index.ts for superadmin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello")
+import express from "express";
+import cors from "cors";
+import adminRouter from "./routes/super-admin.router";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+//v1 routes
+app.use("/api/v1/admin",adminRouter);
+
+
+
+app.listen(3000);
